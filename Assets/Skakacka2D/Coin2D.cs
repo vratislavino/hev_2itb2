884d0bd8ff0fb7471f56f1d6d35d8e6b304a98af
vratislavino <vratislavino@gmail.com>
2023-04-27 12:50:38 +0200

    Coin animation

--- Assets/Skakacka2D/Coin2D.cs
+++ Assets/Skakacka2D/Coin2D.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Coin2D : MonoBehaviour
 {
+    Animator animator;
+
+    private void Start() {
+        animator= GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col) {
 
         var player = col.GetComponent<CharacterMovement2D>();
         if(player != null) {
+            animator.SetTrigger("Fade");
+            Destroy(transform.parent.gameObject, 1f);
             //player.AddCoin();
         } else {
             // narazil do toho nìkdo jiný, neøešíme...
-            Destroy(gameObject);
         }
     }
 }
