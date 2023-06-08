43023d7e9d3120a7e264031c2d00c0bbd89f144b
vratislavino <vratislavino@gmail.com>
2023-06-08 14:19:01 +0200

    Fools project started

---
+++ Assets/Fools/FoolCatcher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoolCatcher : MonoBehaviour
+{
+    Camera cam;
+
+    [SerializeField]
+    private LayerMask foolLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out RaycastHit hit, 50f, foolLayer)) {
+                var fool = hit.collider.GetComponentInParent<Fool>();
+                if(fool != null) {
+                    fool.OnClicked();
+                }
+            }
+
+        }
+    }
+}
